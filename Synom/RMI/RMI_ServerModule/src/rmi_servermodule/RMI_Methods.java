725994705d27e9cd81f12f0b07cb34766405b3d6
Liviu Rujan <liviu_rujan@yahoo.com>
2019-03-22 01:27:08 +0200

    Database on

--- Synom/RMI/RMI_ServerModule/src/rmi_servermodule/RMI_Methods.java
+++ Synom/RMI/RMI_ServerModule/src/rmi_servermodule/RMI_Methods.java
@@ -8,6 +8,7 @@ import java.rmi.RemoteException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import rmi_interfacemodule.RMI_InterfaceModule;
+import java.sql.*;
 
 public class RMI_Methods implements RMI_InterfaceModule {
 
@@ -20,6 +21,8 @@ public class RMI_Methods implements RMI_InterfaceModule {
 
     @Override
     public void sendMessage(String text) throws RemoteException {
+
+        /* Local DB -------------------------------------------------------------------------------------------------------------
         FileWriter writer = null;
         try {
             writer = new FileWriter("C:\\Users\\Liviu\\Desktop\\Logs.txt", true);
@@ -40,6 +43,43 @@ public class RMI_Methods implements RMI_InterfaceModule {
             writer.close();
         } catch (IOException ex) {
             Logger.getLogger(RMI_ServerModule.class.getName()).log(Level.SEVERE, null, ex);
+        }
+*/
+
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        String user = "root";
+        String pass = "fenderice9";
+
+        String var1 = text.substring(42, 43);
+        String var2 = text.substring(60, 64);
+        String var3 = text.substring(96, 100);
+
+        try {
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/vault", user, pass);
+            myStmt = myConn.createStatement();
+            myStmt.executeUpdate("INSERT INTO `vault`.`resources` "
+                    + "(`cpu_usage`, `ram_usage`, `total_ram`) VALUES ('" + var1
+                    + "', '" + var2 + "', '" + var3 + "');");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            if (myRs != null) {
+                try {
+                    myRs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(RMI_Methods.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (myStmt != null) {
+                try {
+                    myStmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(RMI_Methods.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
 
         receivedMessage = text;
